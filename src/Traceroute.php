2707419749a0599d21c8f9cfaf7789140f9ad969
Sterling R <sterling.ross@nearo.com>
2016-09-08 12:27:06 -1000

    Clear any stale traceroute processes

--- src/Traceroute.php
+++ src/Traceroute.php
@@ -11,6 +11,9 @@ class Traceroute
         $host = preg_replace("/[^A-Za-z0-9.]/", "", $host);
 
         $output =  shell_exec("traceroute " . escapeshellarg($host) . " 2>&1");
+        
+        //clear any stale traceroute processes
+        system("killall -q traceroute");
 
         return $output;
     }
