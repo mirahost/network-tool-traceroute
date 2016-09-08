cd981a741b7cd4624a655a8f31c6732119cd2e7d
Sterling R <sterling.ross@nearo.com>
2016-09-08 12:55:13 -1000

    Modified tracroute params to limit time and number of hops

--- src/Traceroute.php
+++ src/Traceroute.php
@@ -10,8 +10,8 @@ class Traceroute
         //TODO update regex to valiadate hostname
         $host = preg_replace("/[^A-Za-z0-9.]/", "", $host);
 
-        $output =  shell_exec("traceroute " . escapeshellarg($host) . " 2>&1");
-        
+        $output =  shell_exec("traceroute -m20 -w1 -q1" . escapeshellarg($host) . " 2>&1");
+
         //clear any stale traceroute processes
         system("killall -q traceroute");
 
